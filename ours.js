9e9aa906b9fe575e968f393ce10e6011663ec30b
Delphine Moutault <dlphn@hotmail.fr>
2020-10-10 18:58:56 +0200

    Ours : Selection NOM et COULEUR OK !

--- ours.js
+++ ours.js
@@ -8,6 +8,36 @@
 //Calcul de nTot
 
 
+//FONCTION name() : Va chercher le name des produits
+const name = (i) => {
+    fetch("http://localhost:3000/api/teddies")
+        .then((resp) => {
+            return resp.json();
+        })
+        .then((all) => {
+            //console.log("ca marche");
+            /* console.log(all);*/ // donne -tableau- avec toutes les données de l'API = TOUS les éléments de TOUS les produits
+            return all;
+        })
+        .then((tabl) => {
+            let obj = tabl[i];
+            /*console.log(obj);  */// donne -objet- TOUS les éléments d'UN produit
+            return obj;
+        })
+        .then((elem) => {
+
+            let nom = elem.name;
+            /*console.log(nom);*/    //donne name du produit
+            return nom;
+        })
+        .then((nom) => {
+            const contents = document.getElementsByClassName("nom-article");    //va chercher les lignes HTML pour ecrire les choix de noms
+            let content = contents[i];
+            content.innerHTML = nom;    //ecrit le nom
+        })
+}
+
+// Affiche la liste des noms des teddies
 fetch("http://localhost:3000/api/teddies")
     .then((resp) => {
         return resp.json();
@@ -22,7 +52,6 @@ fetch("http://localhost:3000/api/teddies")
         for (let i = 0; i < a; i++) {
             name(i);            // appel la fonction name
             console.log("c sure ca marche");
-            
         }
     })
     .catch((err) => {
@@ -30,35 +59,60 @@ fetch("http://localhost:3000/api/teddies")
         console.log(err)
     })
 
-
-//Va chercher le name des produits
-const name = (i) => {
+// FONCTION color va chercher les couleurs d'un ours - i etant le numero dordre des ours
+const color = (i) => {
     fetch("http://localhost:3000/api/teddies")
         .then((resp) => {
             return resp.json();
         })
         .then((all) => {
-            console.log("ca marche");
-           /* console.log(all);*/ // donne -tableau- avec toutes les données de l'API = TOUS les éléments de TOUS les produits
+            //console.log("ca marche");
+            /* console.log(all);*/ // donne -tableau- avec toutes les données de l'API = TOUS les éléments de TOUS les produits
             return all;
         })
         .then((tabl) => {
-            let obj = tabl[i];
+            let obj = tabl[i-1];
             console.log(obj);  // donne -objet- TOUS les éléments d'UN produit
             return obj;
         })
         .then((elem) => {
-            
-                let nom = elem.name;
-                console.log(nom);    //donne name du produit
-                return nom;
+
+            let colors = elem.colors;
+            console.log(colors);    //donne -tableau- avec LES couleurs du produit
+            return colors;
         })
-        .then((nom) => {
-            const contents = document.getElementsByClassName("nom-article");    //va chercher les lignes HTML pour ecrire les choix de noms
-            let content = contents[i];
-            content.innerHTML = nom;    //ecrit le nom
+        .then((colors) => {
+            let nbColors = colors.length;
+            console.log(nbColors); //donne NOMBRE de couleurs du produit
+                        
+            for (let i = 0; i < nbColors; i++) {
+                let color = colors[i];
+                console.log(color);
+                const contents = document.getElementsByClassName("couleur");    //va chercher les lignes HTML pour ecrire les choix de couleurs
+                let content = contents[i+1];
+                content.innerHTML = color;    //ecrit la couleur
+
+            }
+
+            /*const contents = document.getElementsByClassName("couleur"); */   //va chercher les lignes HTML pour ecrire les choix de couleurs
+            //let content = contents[i];
+            //content.innerHTML = color;    //ecrit la couleur
         })
 }
+
+
+
+
+
+// Actions après user selectionne un nom
+let selectionne = document.getElementById('choix-article');
+selectionne.addEventListener('change', function (event) {
+    console.log("selection faite");
+    let votreSelection = document.getElementById("choix-article").selectedIndex;    //numero ordre de l'article
+    console.log(votreSelection);
+    color(votreSelection); // A MODIFIER en couleur, voir avant ce qu affuiche la console
+})
+
 
 
 
@@ -66,41 +120,45 @@ const name = (i) => {
 
 const reqGet = (n,c) => {
 
-fetch("http://localhost:3000/api/teddies", {
+    fetch("http://localhost:3000/api/teddies", {
     
     //method: "GET",
     //data: 'data',
     //dataType: 'json',
     //ContentType: 'application/json'
 
-})
-    .then((resp) => {
+    })
+        .then((resp) => {
         return resp.json();
-    })
-    .then((all) => {
-        console.log("ca marche");
-        const nbreProduits=all.length;
-        console.log(all); // donne -tableau- avec toutes les données de l'API = TOUS les éléments de TOUS les produits
-        console.log(nbreProduits);
-        return all;
-    })
-    .then((tabl) => {
-        let obj = tabl[0];
-        console.log(obj);  // donne -objet- TOUS les éléments d'UN produit
-        return obj;
-    })
-    .then((elem) => {
-        let color = elem.colors;
-        console.log(color); // donne UN élement -color en tableau- d'UN produit
-        let id = elem._id;
-        console.log(id);    // donne UN élement -id- d'UN produit
-    })
-
-    .catch((err) => {
-        console.log("y a un probleme");
-        console.log(err)
-    })
-
+        })
+        .then((all) => {
+            console.log("ca marche");
+            const nbreProduits=all.length;
+            console.log(all); // donne -tableau- avec toutes les données de l'API = TOUS les éléments de TOUS les produits
+            console.log(nbreProduits);
+            return all;
+        })
+        .then((tabl) => {
+            let obj = tabl[0];
+            console.log(obj);  // donne -objet- TOUS les éléments d'UN produit
+            return obj;
+        })
+        .then((elem) => {
+            let color = elem.colors;
+            console.log(color); // donne UN élement -color en tableau- d'UN produit
+            let id = elem._id;
+            console.log(id);    // donne UN élement -id- d'UN produit
+        })
+        .catch((err) => {
+            console.log("y a un probleme");
+            console.log(err)
+        })
 }
 
 //reqGet();
+
+// Affiche le nom de l'ours slectionne--A REVOIR---
+
+
+//let ligne = document.getElementById("selection");
+//ligne.innerHTML = votreSelection;
