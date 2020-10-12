92a95e84bd508e5dda49600742bf095549a8d53e
Delphine Moutault <dlphn@hotmail.fr>
2020-10-12 17:52:49 +0200

    Jusqu'à création bouton pour mise au panier.

--- ours.js
+++ ours.js
@@ -1,5 +1,18 @@
 // JavaScript source code
 
+// Création contenu MENU DEROULANT -quantite- de 1 à 20
+for (let q = 1; q <= 20; q++) {
+    let menuQuantite = document.getElementById("choix-quantite");
+    let optQuantite = document.createElement("option");
+    menuQuantite.appendChild(optQuantite);
+    optQuantite.innerHTML = q;
+}
+
+
+
+
+
+
 //FONCTION name() : Va chercher le name des produits - i etant le numero dordre du produit
 const name = (i) => {
     fetch("http://localhost:3000/api/teddies")
@@ -17,7 +30,6 @@ const name = (i) => {
             return obj;
         })
         .then((elem) => {
-
             let nom = elem.name;
             /*console.log(nom);*/    //donne name du produit
             return nom;
@@ -68,9 +80,13 @@ const color = (i) => {
             return obj;
         })
         .then((elem) => {
-
+            let nom = elem.name;  // Donne nom du produit
+            localStorage.setItem("nom", nom); // Stocke nom
+            let id = elem._id;  // Donne id du produit
+            localStorage.setItem("id", id); // Stocke Id
             let colors = elem.colors;
             console.log(colors);    //donne -tableau- avec LES couleurs du produit
+            localStorage.setItem("objetColors", colors);    // stocke -objet- couleurs du produit
             return colors;
         })
         .then((colors) => {
@@ -105,7 +121,7 @@ const color = (i) => {
         })
 }
 
-// FONCTION affiche AFFICHE le prix, la description, la photo du produit sélectionné - x étant le numéro dordre du nom et y celui de la couleur
+// FONCTION affiche AFFICHE le prix, la description, la photo du produit sélectionné - x étant le numéro dordre du nom , y quantité souhaité
 const affiche = (x,y) => {
     fetch("http://localhost:3000/api/teddies")
         .then((resp) => {
@@ -122,13 +138,19 @@ const affiche = (x,y) => {
             return obj;
         })
         .then((elem) => {
-            let prix = elem.price;
-            let photo = elem.imageURL;
-            let description = elem.description;
-            console.log("Prix = " + prix);    //donne -tableau- avec LES couleurs du produit
+            let prix = elem.price;      //Prix du produit
+            let photo = elem.imageUrl;      // Photo du produit
+            let description = elem.description;     // Description du produit
+            let id = elem._id;      // Id du produit
             let affPrix = document.getElementById("euros");
+            affPrix.innerHTML = prix * y / 100 + " \u20ac";     // Affiche le prix du produit selectionne
+            localStorage.setItem("prixTotal", prix * y / 100);     // Stocke prix total
+            let affDescription = document.getElementById("description");
+            affDescription.innerHTML = description;     // Affiche la description du produit selectionne
+            let affPhoto = document.getElementById("photo");
+            affPhoto.innerHTML = "<img src=" + photo + " width='900' alt='ourson' class='photo' />";     // Affiche la photo du produit selectionne
+            
            
-            affPrix.innerHTML = prix/100+" \u20ac";     // Affiche le prix du produit selectionne
             
             //return colors;
         })
@@ -141,20 +163,38 @@ const affiche = (x,y) => {
 // Actions avec souris-----
 let selectNom = document.getElementById('choix-article');
 selectNom.addEventListener('change', function (event) {   // Action apres selection du nom
+    localStorage.clear();       // Mise à 0 du local storage
     console.log("selection faite");
     document.getElementById("choix-couleur").selectedIndex = 0; // Forcer le menu COULEUR sur la 1re option
+    document.getElementById("choix-quantite").selectedIndex = 0; // Forcer le menu QUANTITE sur la 1re option
     let selectionNom = document.getElementById("choix-article").selectedIndex;    //numero ordre de l'article
+    localStorage.setItem("indexNom", selectionNom);   // stocke numero ordre du nom
     console.log(selectionNom);
     color(selectionNom); // Appel la fonction COLOR
+    affiche(selectionNom,1);   // Appel la fonction AFFICHE
 });
 let selectColor = document.getElementById('choix-couleur');
 selectColor.addEventListener('change', function (event) {   // Action apres selection de la couleur
     console.log("selection couleur faite");
-    let selectionColor = document.getElementById("choix-couleur").selectedIndex;    //numero ordre de la couleur
-    console.log("Color : " + selectionColor);
-    let selectionNom = document.getElementById("choix-article").selectedIndex; 
-    console.log("Nom : " + selectionNom);
-    affiche(selectionNom, selectionColor);   // Appel la fonction AFFICHE
+    // A REVOIR pour mettre dans localStorage la couleur choisie -----------------
+    //let selectionColor = document.getElementById("choix-couleur").selectedIndex;    //numero ordre de la couleur
+    //let color = document.getElementById("choix-couleur").selectedValue; 
+    //console.log("Ta couleur : "+color);
+    //localStorage.setItem("indexCouleur", selectionColor);   // stocke numero ordre de la couleur
+    console.log(localStorage);
+});
+let selectQuantite = document.getElementById('choix-quantite');
+selectQuantite.addEventListener('change', function (event) {   // Action apres selection de la QUANTITE
+    console.log("selection quantite faite");
+    let selectionQuantite = document.getElementById("choix-quantite").selectedIndex + 1;    //numero ordre de la QUANTITE
+    localStorage.setItem("quantite", selectionQuantite);     // Stockage de la quantite
+    affiche(localStorage.getItem("indexNom"), selectionQuantite);   // Appel la fonction AFFICHE
+});
+let selectPanier = document.getElementById('bouton-panier');
+selectPanier.addEventListener('click', function (event) {   // Action apres clic sur BOUTON PANIER
+    console.log("Validation PANIER");
+    console.log(localStorage);
+    
 });
 
 // Quand reload page, afficher -Selectionnez- dans le menu des noms  
